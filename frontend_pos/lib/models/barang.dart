b1ac595d96ebc83472c29cc355bbaa40866aa424
putu tirta <pututirta408@gmail.com>
2026-07-11 12:37:48 +0800

    feat: add optional description field to Barang model and update JSON parsing

--- frontend_pos/lib/models/barang.dart
+++ frontend_pos/lib/models/barang.dart
@@ -1,6 +1,7 @@
 class Barang {
   final int id;
   final String namaBarang;
+  final String? deskripsi;
   final int harga;
   final int stok;
   final String? imageUrl;
@@ -8,6 +9,7 @@ class Barang {
   Barang({
     required this.id,
     required this.namaBarang,
+    this.deskripsi,
     required this.harga,
     required this.stok,
     this.imageUrl,
@@ -17,6 +19,7 @@ class Barang {
     return Barang(
       id: json['id'],
       namaBarang: json['nama_barang'],
+      deskripsi: json['deskripsi'],
       harga: json['harga'] is String ? int.parse(json['harga']) : json['harga'],
       stok: json['stok'] is String ? int.parse(json['stok']) : json['stok'],
       imageUrl: json['image_url'],
